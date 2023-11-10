ee19cbcee611cd343509aa518d1694236a7eed44
fatemaranapurwala <143012337+fatemaranapurwala@users.noreply.github.com>
2023-11-10 10:00:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC_7CQd5k6PSat60JUdXfsVq87J191anT0",
+  authDomain: "project83-61e54.firebaseapp.com",
+  projectId: "project83-61e54",
+  storageBucket: "project83-61e54.appspot.com",
+  messagingSenderId: "277077756538",
+  appId: "1:277077756538:web:573b9b7761b2990f9709c3"
+};
+
+
